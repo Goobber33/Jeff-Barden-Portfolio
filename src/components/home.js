9554073d2a0f0b322217b0ba2867236cc6a3073a
Kyle <kcparks1234@gmail.com>
2023-09-13 12:09:50 -0700

    Added background image and h1 and p text which are centered.

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import shopImage from '../images/carpenter.jpg'; // Adjust the relative path as needed
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Welcome to Jeff's Carpentry</h1>
-      <p>Your vision, our creation.</p>
-    </div>
-  );
+    const backgroundStyle = {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${shopImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        margin: 0,
+        padding: 0,
+    };
+
+    const centerTextStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%', // This will be relative to parent div height
+        textAlign: 'center',
+        color: 'white'
+    };
+
+    const h1Style = {
+        fontSize: '4rem', // Adjust the value as needed
+    };
+
+    const pStyle = {
+        fontSize: '1.5rem', // Adjust the value as needed
+    };
+
+    return (
+        <div style={backgroundStyle} className="d-flex justify-content-center align-items-center">
+            <div style={centerTextStyle} className="text-white">
+                <h1 style={h1Style}>New Wood Design</h1>
+                <p style={pStyle}>Your vision, our creation.</p>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
