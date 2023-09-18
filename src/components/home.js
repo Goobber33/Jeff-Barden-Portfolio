800421c1932b2baa92bb6ca3f296c8e053fdb1c8
Kyle <kcparks1234@gmail.com>
2023-09-18 16:52:00 -0700

    Added photo's the customer provided.

--- src/components/home.js
+++ src/components/home.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect, useRef } from 'react';
 import '../styles/Home.css'
 import shopImage from '../images/carpenter.jpg';
 import logo from '../images/logo.png';
-import img1 from '../images/img1.jpg';
-import img2 from '../images/img2.jpg';
+import img1 from '../images/img1.JPG';
+import img2 from '../images/img2.JPG';
 import img3 from '../images/img3.jpg';
 import img4 from '../images/img4.jpg';
-import img5 from '../images/img5.png';
+import img5 from '../images/img5.jpg';
+import img6 from '../images/img6.jpg';
+import img7 from '../images/img7.jpg';
+import img8 from '../images/img8.jpg';
+import img9 from '../images/img9.jpg';
+import img10 from '../images/img10.jpg';
 
 const Home = () => {
-  const allImages = [img1, img2, img3, img4, img5];
+  const allImages = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
   const [startIndex, setStartIndex] = useState(0);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [modalImage, setModalImage] = useState(null);
